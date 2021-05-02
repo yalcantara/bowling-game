0d649e06ada9cc2bc293ec28abe34d1f7ca0345a
Yaison Alcantara <yaison1708@gmail.com>
2021-05-02 15:02:13 -0400

    Adding comments to interfaces.

--- src/main/java/com/jobsity/bowling/game/IBowlingGame.java
+++ src/main/java/com/jobsity/bowling/game/IBowlingGame.java
@@ -1,8 +1,30 @@
 package com.jobsity.bowling.game;
 
+/**
+ * Defines the very basics of a streaming Bowling Game by accepting players shot's
+ * value on the go. Since there are many types of Bowling Games, this interface is
+ * simple enough to allow different implementations to set their own rules & strategies.
+ */
 public interface IBowlingGame {
 	
+	/**
+	 * Saves the player shot's value in the current game, adding to the total list of shots.
+	 * @param player the player's id or unique name.
+	 * @param val the shot's value: 1-10, X, F or /.
+	 */
 	public void append(String player, String val);
+	
+	/**
+	 * Returns the current score for the given player and frame.
+	 * @param player the player's id or unique name.
+	 * @param frame the frame to query.
+	 * @return the accumulated score of the player at the given frame.
+	 */
 	public int getScore(String player, int frame);
+	
+	/**
+	 * Prints to the output console the current state of the game, including the
+	 * names of the players, frames and score.
+	 */
 	public void print();
 }
--- src/main/java/com/jobsity/bowling/game/IGameParser.java
+++ src/main/java/com/jobsity/bowling/game/IGameParser.java
@@ -3,15 +3,15 @@ package com.jobsity.bowling.game;
 import com.jobsity.bowling.model.PlayerEntry;
 
 /**
- * Defines some basic method for general parsing capabilities for the bowing program. This simple
- * design allows extending for multiple parsing sources, such as: stream, web, output of another
- * program, etc..
+ * Defines some basic method for general parsing capabilities for the bowing program.
+ * This simple design allows extending for multiple parsing sources, such as: stream,
+ * web, output of another program, etc..
  */
 public interface IGameParser extends AutoCloseable {
 	
 	/**
-	 * This method is called when the bowling programs starts and before calling the {@link IGameParser#next()}
-	 * method.
+	 * This method is called when the bowling programs starts and before calling
+	 * the {@link IGameParser#next()} method.
 	 */
 	public void init();
 	
